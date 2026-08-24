397b5a6ac3846077ff0a44a79a6a2933e5a799d4
AdityaSarin1802 <sarin.aditya18@gmail.com>
2026-08-24 09:55:02 +0530

    firse commit

--- promises.js
+++ promises.js
@@ -1,5 +1,5 @@
 const promise1 = new Promise((resolve, reject) => {
-    let success = false
+    let success = true
     if (success) {
         resolve({
             id: 2930209, username: "John Doe"
@@ -16,20 +16,34 @@ const promise1 = new Promise((resolve, reject) => {
 //     console.log(error.message);
 // })
 
-const promise2 = new Promise((resolve, reject) => {
-    let success = true
-    if (success) {
-        resolve({
-            id: 2930210, username: "Kamala Harris"
-        });
-    }
-    else {
-        reject(new Error("Data not fetched"));
-    }
-})
+// const promise2 = new Promise((resolve, reject) => {
+//     let success = true
+//     if (success) {
+//         resolve({
+//             id: 2930210, username: "Kamala Harris"
+//         });
+//     }
+//     else {
+//         reject(new Error("Data not fetched"));
+//     }
+// })
 
-promise2.then((response)=>{
-    console.log(response);
+// // promise2.then((response)=>{
+// //     console.log(response);
+// // }).catch((error)=>{
+// //     console.log(error.message);
+// // })
+
+// Promise.any([promise1, promise2]).then((response)=>{
+//     console.log(response2.username);
+// }).catch((error)=>{
+//     console.log(error.message);
+// })
+
+promise1.then((response)=>{
+    return response
+}).then((response2)=>{
+    console.log(response2.username);
 }).catch((error)=>{
     console.log(error.message);
 })
