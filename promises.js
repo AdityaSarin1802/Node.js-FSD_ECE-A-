83037efab70403c9e31dc2c527bc4b73ca8841ae
AdityaSarin1802 <sarin.aditya18@gmail.com>
2026-08-24 09:21:08 +0530

    error implementation

--- promises.js
+++ promises.js
@@ -1,11 +1,17 @@
 const promise1 = new Promise((resolve, reject) => {
-    let success = true
+    let success = false
     if (success) {
         resolve({
             id: 2930209, username: "John Doe"
-        })
+        });
     }
     else {
-        reject(new Error("Data not fetched"))
+        reject(new Error("Data not fetched"));
     }
+})
+
+promise1.then((response)=>{
+    console.log(response);
+}).catch((error)=>{
+    console.log(error);
 })
