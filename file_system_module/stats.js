45d95889b76fc696f6cb705b7590fc9432704c2a
AdityaSarin1802 <sarin.aditya18@gmail.com>
2026-09-01 12:11:25 +0530

    stats.birthtime

--- file_system_module/stats.js
+++ file_system_module/stats.js
@@ -1,8 +1,10 @@
-const fs=reqquire('fs')
+const fs=require('fs')
 fs.stat('notes.txt', (err, stats) => {
     if (err) {
         console.error(err);
         return;
     }
-    console.log('Information about [notes.txt]:', stats);
+    // console.log('Information about [notes.txt]:', stats)
+    console.log("size of the file [notes.txt]:", stats.size)
+    console.log("Creation time of the file [notes.txt]:", stats.birthtime.toISOString())
 })
