e70b705acdc1c234908eac9ed84af55f93d0d044
AdityaSarin1802 <sarin.aditya18@gmail.com>
2026-09-07 09:23:27 +0530

    timeout

--- file_system_module/node_watch/watch.js
+++ file_system_module/node_watch/watch.js
@@ -3,3 +3,7 @@ fs.watch("intro.txt",(eventType, filename)=>{
     console.log("Event:", eventType)
     console.log("Filename:", filename)
 })
+
+setTimeout(()=>{
+    console.log("Watcher Closed")
+}, 5000)
