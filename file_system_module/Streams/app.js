737eca4e2ee6abf13c0955bf2f61ba6c444dc9d4
AdityaSarin1802 <sarin.aditya18@gmail.com>
2026-09-07 10:03:57 +0530

    chunk

--- file_system_module/Streams/app.js
+++ file_system_module/Streams/app.js
@@ -1,4 +1,8 @@
 //Readable Streams
 
 import fs from "fs";
-const readStream = fs.createReadStream("./input.txt", "utf-8");
+const readStream = fs.createReadStream("./input.txt", "utf-8")
+readStream.on("data", (chunk) => {
+    console.log("Data Received");
+    console.log("Data",chunk)
+})
