c8f9e5fbd63a5123c80624810b83104c82c168aa
AdityaSarin1802 <sarin.aditya18@gmail.com>
2026-09-08 12:06:50 +0530

    phases

--- micro_vs_macro.js
+++ micro_vs_macro.js
@@ -4,3 +4,15 @@ setTimeout(() => console.log("MacroQueue"), 0);
 Promise.resolve().then(() => {
     console.log("MicroQueue")
 });
+
+//no of queues in nodejs
+// 1. MicroQueue
+// 2. MacroQueue
+
+//no of phases in nodejs
+// 1. Timers
+// 2. I/O Callbacks
+// 3. Check
+// 4. Close Callbacks
+// 5. Poll
+// 6. Idle, Prepare
