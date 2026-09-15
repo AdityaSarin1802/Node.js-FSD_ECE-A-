61c75e5834ad7895c53f3a6118c3849fae09a197
AdityaSarin1802 <sarin.aditya18@gmail.com>
2026-09-15 12:25:45 +0530

    req

--- http_module/routing.js
+++ http_module/routing.js
@@ -1,9 +1,12 @@
 import http from 'http'
 const server=http.createServer((req,res)=>{
     console.log("Yoooooooö");
+    console.log(req.url)
     res.end("Hello from the server");
+    //if(req.url === "/")
+    //
 })
 
 server.listen(8000,()=>{
-    console.log("Server is listening on port 8000");
+    console.log("Server is running on port http://localhost:8000");
 })
