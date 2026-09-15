e9f00161002228bcb4bbe970195396a1aa332093
AdityaSarin1802 <sarin.aditya18@gmail.com>
2026-09-15 12:19:08 +0530

    server listen

--- http_module/routing.js
+++ http_module/routing.js
@@ -3,3 +3,7 @@ const server=http.createServer((req,res)=>{
     console.log("Yoooooooö");
     res.end("Hello from the server");
 })
+
+server.listen(8000,()=>{
+    console.log("Server is listening on port 8000");
+})
