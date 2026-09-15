c42fd5f740d72f1e7a4362e2feeac4a887686be4
AdityaSarin1802 <sarin.aditya18@gmail.com>
2026-09-15 12:29:59 +0530

    response

--- http_module/routing.js
+++ http_module/routing.js
@@ -3,10 +3,11 @@ const server=http.createServer((req,res)=>{
     console.log("Yoooooooö");
     console.log(req.url)
     res.end("Hello from the server");
-    //if(req.url === "/")
-    //
+    if(req.url === "/"){
+        res.end("Ghr se Namaste");
+    }
 })
 
-server.listen(8000,()=>{
-    console.log("Server is running on port http://localhost:8000");
+server.listen(8000,"127.0.0.1",()=>{
+    console.log("Server is running on port http://127.0.0.1:8000");
 })
