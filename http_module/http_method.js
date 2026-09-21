a4d818ec32c88e383eb108f98a5e0ddd56d99bcb
AdityaSarin1802 <sarin.aditya18@gmail.com>
2026-09-21 09:16:33 +0530

    server

--- http_module/http_method.js
+++ http_module/http_method.js
@@ -4,3 +4,24 @@
 // // patch- to partially update data
 // delete- to delete data
 
+const http = require('http');
+const server= http.createServer((req,res)=>{
+
+    if(req.method==='GET'){
+        res.end('This is a GET request')
+    }else if(req.method==='POST'){
+        res.end('This is a POST request')
+    }else if(req.method==='PUT'){
+        res.end('This is a PUT request')
+    }else if(req.method==='PATCH'){
+        res.end('This is a PATCH request')
+    }else if(req.method==='DELETE'){
+        res.end('This is a DELETE request')
+    }else{
+        res.end('Invalid request method')
+    }
+});
+
+server.listen(5500, '127.0.0.1', () => {
+    console.log('Server is running on http://127.0.0.1:5500/');
+});
