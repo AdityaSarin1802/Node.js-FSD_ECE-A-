c9f18dc133a2dd21277de5c60d0c33d0fa803273
AdityaSarin1802 <sarin.aditya18@gmail.com>
2026-09-21 09:42:22 +0530

    strinigy

--- http_module/http_method.js
+++ http_module/http_method.js
@@ -6,25 +6,27 @@
 
 const http = require('http');
 const server= http.createServer((req,res)=>{
-    
-    res.statusCode=200;
+
+    // res.statusCode=200;
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
 
-    if(req.method==='GET'){
-        res.end('This is a GET request')
-    }else if(req.method==='POST'){
-        res.end('This is a POST request')
-    }else if(req.method==='PUT'){
-        res.end('This is a PUT request')
-    }else if(req.method==='PATCH'){
-        res.end('This is a PATCH request')
-    }else if(req.method==='DELETE'){
-        res.end('This is a DELETE request')
-    }else{
-        res.end('Invalid request method')
-    }
+    res.end(JSON.stringify(require('./config.json')));
+
+    // if(req.method==='GET'){
+    //     res.end('This is a GET request')
+    // }else if(req.method==='POST'){
+    //     res.end('This is a POST request')
+    // }else if(req.method==='PUT'){
+    //     res.end('This is a PUT request')
+    // }else if(req.method==='PATCH'){
+    //     res.end('This is a PATCH request')
+    // }else if(req.method==='DELETE'){
+    //     res.end('This is a DELETE request')
+    // }else{
+    //     res.end('Invalid request method')
+    // }
 });
 
 server.listen(5500, '127.0.0.1', () => {
