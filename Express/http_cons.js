749830165ec71af33fbd0b2ab3474e4a072fff4d
AdityaSarin1802 <sarin.aditya18@gmail.com>
2026-09-22 11:42:58 +0530

    server

---
+++ Express/http_cons.js
@@ -0,0 +1,19 @@
+//get- to read data
+//post- to send data
+// // put- to update data
+// // patch- to partially update data
+// delete- to delete data
+
+const http =require("http")
+
+const server= http.createServer((req,res)=>{
+    if(req.method === "GET" && req.url === "/"){
+        res.end("GET Request")
+    } else if(req.method === "POST" && req.url === "/"){
+        res.end("POST Request")
+    }
+})
+
+server.listen(3000,(req,res)=>{
+    console.log("Server is running on the PORT: 3000")
+})
