ea5f07dc05ae92a91952058ac71d32357a78b065
AdityaSarin1802 <sarin.aditya18@gmail.com>
2026-09-22 12:27:57 +0530

    listen

--- Express/app.js
+++ Express/app.js
@@ -6,3 +6,8 @@ const app= express()//instance
 app.get("/home",(req,res)=>{
     res.send("Express mai aapka swagat nhi hai")
 })
+
+const PORT = 3000
+app.listen(PORT, ()=>{
+    console.log("Server chl rha hai usko pakkad lo")
+})
